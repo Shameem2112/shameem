f920fc616337f0740f437fcce08722b1dc8d40f4
Shameem <142601407+Shameem2112@users.noreply.github.com>
2025-09-04 15:40:28 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,35 +1,64 @@
- var tl = gsap.timeline()
- tl.from("#intro h1, #place h3, #nav h3,#place h2",{
-     y:-100,
-    duration:1.5,
-    delay:1,
-    opacity:0,
-    stagger:0.5,
+var tl = gsap.timeline()
 
-
-
+tl.from("#nav h3, #intro h4, #intro h1, #hero-content p, #place", {
+    y: -50,
+    duration: 1,
+    delay: 0.5,
+    opacity: 0,
+    stagger: 0.2
 })
-tl.from("#hero-img",{
-    scale:0,
-    opacity:0,
-    duration:1.5,
-})
-tl.to("#place h2",{
-    y:10,
-    repeat:-1,
-    duration:1,
-    yoyo:true,
 
+tl.from("#hero-img", {
+    scale: 0,
+    opacity: 0,
+    duration: 1,
+    ease: "back.out(1.7)"
 })
-// gsap.from("#technical h4",{
-//     x:-10,
-//     repeat:-1,
-//     duration:1,
-//     yoyo:true,
-// })
-// gsap.to("#education h3",{
-//     y:200,
-//     repeat:-1,
-//     duration:1.5,
-//     yoyo:true,
-// })
+
+tl.to("#place h2", {
+    y: 10,
+    repeat: -1,
+    duration: 1,
+    yoyo: true
+})
+
+gsap.from("#technical h3", {
+    x: -20,
+    repeat: -1,
+    duration: 1.5,
+    yoyo: true
+})
+
+gsap.from("#education h3", {
+    x: -20,
+    repeat: -1,
+    duration: 1.5,
+    yoyo: true
+})
+
+// Additional animations for the new design
+gsap.from(".work-item", {
+    scrollTrigger: {
+        trigger: ".work-item",
+        start: "top 85%",
+        end: "bottom 60%",
+        toggleActions: "play none none none"
+    },
+    opacity: 0,
+    y: 50,
+    stagger: 0.2,
+    duration: 1
+})
+
+gsap.from(".education-item", {
+    scrollTrigger: {
+        trigger: ".education-item",
+        start: "top 85%",
+        end: "bottom 60%",
+        toggleActions: "play none none none"
+    },
+    opacity: 0,
+    y: 30,
+    stagger: 0.2,
+    duration: 1
+})
